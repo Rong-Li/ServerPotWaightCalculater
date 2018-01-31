0791a4f0e7373eb2f81edf6bb56c8611d8492701
lirongl <lirongl@sfu.ca>
2018-01-30 22:57:12 -0800

    ThirdCommit

--- app/src/main/java/com/example/lirongl/servingsizecalculator/ThirdActivity.java
+++ app/src/main/java/com/example/lirongl/servingsizecalculator/ThirdActivity.java
@@ -4,14 +4,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class ThirdActivity extends AppCompatActivity {
 
     private String theName;
     private int theWeight;
+    private int totalWeight;
+    private int foodWeight;
+    private int servingNumber = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +26,7 @@ public class ThirdActivity extends AppCompatActivity {
         ExtractData();
         setThePot();
         exitTheActivity();
+        computeTheResult();
     }
 
 
@@ -34,6 +41,47 @@ public class ThirdActivity extends AppCompatActivity {
         firstText.setText(theName);
         TextView secondText = (TextView) findViewById(R.id.weightwieghtID);
         secondText.setText("" + theWeight);
+    }
+
+    private void computeTheResult() {
+        final EditText inputOne = (EditText) findViewById(R.id.withFood);
+        final EditText inputTwo = (EditText) findViewById(R.id.servingNumbers);
+
+        inputOne.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String weightInString = inputOne.getText().toString();
+                totalWeight = Integer.parseInt(weightInString);
+                foodWeight = totalWeight - theWeight;
+                TextView textview = (TextView) findViewById(R.id.foodWeightID);
+                textview.setText(""+foodWeight);
+            }
+        });
+
+        inputTwo.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String servingInString = inputTwo.getText().toString();
+                servingNumber = Integer.parseInt(servingInString);
+                if (servingNumber > 0)
+                {int finalWeightinInteger = foodWeight / servingNumber;
+                TextView finalWeight = (TextView) findViewById(R.id.finalID);
+                finalWeight.setText("" + finalWeightinInteger);
+                }
+            }
+        });
+
+
     }
 
     private void exitTheActivity() {
